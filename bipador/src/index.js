4985a6a68a9292d519488785ebda47696df99f34
GhabrielBender <ghabrielbmutti@gmail.com>
2022-11-03 11:59:11 -0300

    add: inital config

---
+++ bipador/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import Router from "./routes";
+import "./global.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <Router /> */}
+    <div>Hello World!</div>
+  </React.StrictMode>
+);
